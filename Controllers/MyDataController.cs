c596518f17178324fbd614b6dee97dadef75d14a
Mohamed Samir <mosamircs@gmail.com>
2021-10-03 15:03:56 +0200

    Making my own API with my Data

---
+++ Controllers/MyDataController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.Controllers
+{//.net5
+    [ApiController]
+    [Route("[controller]")]
+    public class MyDataController : ControllerBase
+    {
+        private static readonly string[] Abilties = new[]
+        {
+            ".Net","C#","HTML","CSS","JavaScript","Algorithms","DataStructures","OOP","DesignPatterns","SOLID"
+        };
+
+        private readonly ILogger<MyDataController> _logger;
+
+        public WeatherController(ILogger<MyDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<MyData> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new MyData
+            {
+                Name = "Mohamed Samir",
+                YearOfBirth = 1996,
+                Abilties=
+            })
+            .ToArray();
+        }
+    }
+}
