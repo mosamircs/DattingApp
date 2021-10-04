923fcdced38fc862097ed0405bfc70288cc29cdc
Mohamed Samir <mosamircs@gmail.com>
2021-10-05 01:07:49 +0200

    created angular viewer for our dating app and work

---
+++ API/Controllers/MyDataController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.Controllers
+{//.net5
+    [ApiController]
+    [Route("[controller]")]
+    public class MyDataController : ControllerBase
+    {
+        private static readonly string[] Abilties = new[]
+        {
+            ".Net","C#","HTML","CSS","JavaScript","Algorithms","DataStructures","OOP","DesignPatterns","SOLID"
+        };
+        private static readonly string[] Name = new[]
+        {
+            "Moahemd","Samir","amin","test2"
+        };
+        private static readonly int [] YearOfBirth=new []
+        {
+            1997,1996,1967,2000
+        };
+        private readonly ILogger<MyDataController> _logger;
+
+        public MyDataController(ILogger<MyDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]//End point 
+        //that we get when HTTP GET request data from API Server
+        public IEnumerable<MyData> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new MyData
+            {
+                Name = Name[rng.Next(Name.Length)],
+                YearOfBirth = YearOfBirth[rng.Next(YearOfBirth.Length)],
+                Abilties = Abilties[rng.Next(Abilties.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() => await _context.Users.ToListAsync();
+        //api/users/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id )
+        {
+            return await
+             _context.Users.FindAsync(id);
+        }
+    }
+}
